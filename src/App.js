dfcad76ea6232d4c469f4c3d6405cf5a249caf7a
schillingcodes <tamakuchiko@gmail.com>
2021-02-15 18:40:34 -0500

    Implement useContext for user updates

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
+import React, {useState, useEffect, useContext, createContext} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {signUpWithEmailPassword, signOut} from './email.js';
+import useFirebaseAuthentication from './useFirebaseAuthentication.js';
+import {FirebaseContext} from './FirebaseProvider.js';
 
-function App() {
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+import "firebase/auth";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA50OTyAvimezI7L7pqT1G5LIDBZ_7Xegw",
+  authDomain: "web-playground-41e27.firebaseapp.com",
+  projectId: "web-playground-41e27",
+  storageBucket: "web-playground-41e27.appspot.com",
+  messagingSenderId: "159707765797",
+  appId: "1:159707765797:web:1bc4b921fc4c9f9c0f73cc",
+  measurementId: "G-Z48FQGNM62"
+};
+
+// Intialize Firebase if it has not been previously initialized
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+
+function App(props) {
+  const firebase = useContext(FirebaseContext);
+  const authUser = useFirebaseAuthentication(firebase);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Yoyo</p>
+        Hello, {getUid(authUser)}
       </header>
       <button onClick={signUpWithEmailPassword}>test</button>
       <button onClick={signOut}>Sign Out</button>
     </div>
   );
+}
+
+function getUid(authUser) {
+  if (authUser) return authUser.uid;
+  else return "Stranger";
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,52 +4,13 @@ import './index.css';
 import App from './App';
 import LoginForm from './LoginForm';
 import reportWebVitals from './reportWebVitals';
-
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
-
-import "firebase/auth";
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA50OTyAvimezI7L7pqT1G5LIDBZ_7Xegw",
-  authDomain: "web-playground-41e27.firebaseapp.com",
-  projectId: "web-playground-41e27",
-  storageBucket: "web-playground-41e27.appspot.com",
-  messagingSenderId: "159707765797",
-  appId: "1:159707765797:web:1bc4b921fc4c9f9c0f73cc",
-  measurementId: "G-Z48FQGNM62"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    var uid = user.uid;
-    ReactDOM.render(
-      <p>Hello, {uid}</p>,
-      document.getElementById('profile')
-    );
-    // ...
-  } else {
-    // User is signed out
-    ReactDOM.render(
-      <p>Hello, Stranger</p>,
-      document.getElementById('profile')
-    );
-  }
-});
+import {FirebaseProvider} from './FirebaseProvider.js';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseProvider>
     <App />
     <LoginForm />
-  </React.StrictMode>,
+  </FirebaseProvider>,
   document.getElementById('root')
 );
 
